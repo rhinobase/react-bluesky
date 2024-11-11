b9195c4faa8f75a7b1453d82672cb54c2de1c208
Gajendra-singh-r <137156034+Gajendra-singh-r@users.noreply.github.com>
2024-11-11 17:48:56 +0530

    feat(docs): added docs

---
+++ apps/docs/app/(docs)/layout.tsx
@@ -0,0 +1,22 @@
+import glob from "fast-glob";
+import type { PropsWithChildren } from "react";
+import { Layout } from "../../components/Layout";
+import type { Section } from "../../components/SectionProvider";
+import { Providers } from "./providers";
+
+export default async function RootLayout(props: PropsWithChildren) {
+  const pages = await glob("**/*.mdx", { cwd: "./app/(docs)" });
+  const allSectionsEntries = (await Promise.all(
+    pages.map(async (filename) => [
+      `/${filename.replace(/(^|\/)page\.mdx$/, "")}`,
+      (await import(`./${filename}`)).sections,
+    ]),
+  )) as Array<[string, Array<Section>]>;
+  const allSections = Object.fromEntries(allSectionsEntries);
+
+  return (
+    <Providers>
+      <Layout allSections={allSections}>{props.children}</Layout>
+    </Providers>
+  );
+}
---
+++ apps/docs/next.config.mjs
@@ -0,0 +1,38 @@
+import nextMDX from "@next/mdx";
+import { composePlugins, withNx } from "@nx/next";
+import withSearch from "./mdx/search.mjs";
+import { recmaPlugins } from "./mdx/recma.mjs";
+import { rehypePlugins } from "./mdx/rehype.mjs";
+import { remarkPlugins } from "./mdx/remark.mjs";
+import pk from "../../package.json" assert { type: "json" };
+
+const withMDX = nextMDX({
+  options: {
+    remarkPlugins,
+    rehypePlugins,
+    recmaPlugins,
+  },
+});
+
+/**
+ * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
+ **/
+const nextConfig = {
+  nx: {
+    // Set this to true if you would like to use SVGR
+    // See: https://github.com/gregberge/svgr
+    svgr: false,
+  },
+  pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
+  experimental: {
+    optimizePackageImports: ["@rafty/ui"],
+  },
+  reactStrictMode: false,
+  env: {
+    NEXT_PUBLIC_VERSION: pk.version,
+  },
+};
+
+const plugins = [withSearch, withMDX, withNx];
+
+export default composePlugins(...plugins)(nextConfig);
